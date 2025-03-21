d788d72d7126f56d9675f78d34885414df54e556
naxor <naxordeve@gmail.com>
2025-03-21 18:38:14 +0200

    Update fb.js

--- commands/fb.js
+++ commands/fb.js
@@ -5,10 +5,10 @@ Command({
     cmd_name: 'fb',
     category: 'downloader',
     desc: 'Download fb videos'
-})(async (msg, args) => {
-    if (!args[0]) return msg.reply('Please provide an fb url!\eg: !fb https://www.facebook.com/watch?v=123456789');
+})(async (msg) => {
+    if (!msg.args[0]) return msg.reply('Please provide an fb url!\eg: !fb https://www.facebook.com/watch?v=123456789');
         await msg.react('⏳');
-        const res = await axios.get(`https://diegoson-naxordeve.hf.space/facebook?url=${args[0]}`);
+        const res = await axios.get(`https://diegoson-naxordeve.hf.space/facebook?url=${msg.args[0]}`);
         if (res.data.success && res.data.data.download?.url) {
             const p = await msg.reply('*Downloading...*');
                 await msg.send({ video: { url: res.data.data.download.url }, caption: `*Quality:* ${res.data.data.quality}`, mimetype: 'video/mp4' });
