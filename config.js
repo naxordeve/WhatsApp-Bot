bcf2828715f1ac3d0d8c20e573baf59e44be7904
naxor <naxordeve@gmail.com>
2025-03-21 17:06:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = {
     ownerNumber: process.env.OWNER_NUMBER || '27686881509',
     mods: (process.env.MODS || '').split(',').filter(Boolean),
     botName: process.env.BOT_NAME || 'AquaSeek',
+    session_id: process.env.SESSION_ID || '',
     footer: process.env.FOOTER || '© wabot',
     lang: process.env.LANG || 'en',
     workType: process.env.WORK_TYPE || 'public'
