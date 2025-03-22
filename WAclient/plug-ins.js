c2f8a11bcb428a5f710ded5dc53972adb2ae40df
naxor <naxordeve@gmail.com>
2025-03-22 15:04:02 +0200

    Update and rename commands.js to plug-ins.js

--- WAclient/plug-ins.js
+++ WAclient/plug-ins.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 function plugins() {
-    const com = path.join(__dirname, '../commands');
+    const com = path.join(__dirname, 'commands');
     const c = fs.readdirSync(com).filter(file => file.endsWith('.js'));
     for (const file of co) {
         require(path.join(com, file));
-    } console.log('Commands loaded');
+    } console.log('Commands loaded✅');
 }
 
 module.exports = { plugins };
