5c5ddaf406f44120bebf513c397276a0c7ac5a33
naxor <naxordeve@gmail.com>
2025-03-22 15:39:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 module.exports = {
     prefix: new RegExp(process.env.PREFIX || '^[!/.#]'),
-    ownerNumber: process.env.OWNER_NUMBER || '27686881509',
-    mods: (process.env.MODS || '').split(',').filter(Boolean),
+    owner_num: process.env.OWNER_NUMBER || '27686881509',
+    mods: (process.env.MODS || '27686881509').split(',').filter(Boolean),
     botName: process.env.BOT_NAME || 'AquaSeek',
-    session_id: process.env.SESSION_ID || 'c_IRIS_Hhpa1c_IRIS_4NHE=',
+    SESSION_ID: process.env.SESSION_ID || 'c_IRIS_Hhpa1c_IRIS_4NHE=',
     footer: process.env.FOOTER || '© wabot',
     lang: process.env.LANG || 'en',
     workType: process.env.WORK_TYPE || 'public'
