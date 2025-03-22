8686299704bdf42530ef909c8d4ee1d6785a2c13
naxor <naxordeve@gmail.com>
2025-03-22 16:17:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ module.exports = {
     owner_num: process.env.OWNER_NUMBER || '27686881509',
     mods: (process.env.MODS || '27686881509').split(',').filter(Boolean),
     botName: process.env.BOT_NAME || 'AquaSeek',
-    SESSION_ID: process.env.SESSION_ID || 'c_IRIS_Hhpa1c_IRIS_4NHE=',
+    SESSION_ID: process.env.SESSION_ID || '',
+    grok_api: process.env.GROK_API || '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',
     footer: process.env.FOOTER || '© wabot',
     lang: process.env.LANG || 'en',
     workType: process.env.WORK_TYPE || 'public'
