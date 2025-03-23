6e7376c82e73a7eb9a264b774e5b6708fb5870b4
naxor <naxordeve@gmail.com>
2025-03-23 08:38:28 +0200

    naxor

---
+++ WAclient/commands/sticker.js
@@ -0,0 +1,20 @@
+const { Command } = require('../lib/command');
+const { makeSticker } = require('../lib/Sticker');
+
+Command({
+    cmd_name: 'sticker',
+    category: 'media',
+    desc: 'Convert image/video to sticker'
+})(async (msg, conn) => {
+    const quoted = msg.quoted ? msg.quoted : msg;
+    const mime = quoted.type || '';
+    if (!/image|video|gif/.test(mime)) return msg.reply('_Reply to an image, video, or GIF_');
+    msg.reply('Creating stk...');
+    const mediaData = await quoted.download();
+    let buffer = Buffer.from([]);
+    for await (const chunk of mediaData) buffer = Buffer.concat([buffer, chunk]);
+    const sticker = await makeSticker(buffer, mime);
+    if (!sticker) return msg.reply('err');
+    await msg.send({ sticker });
+});
+      
