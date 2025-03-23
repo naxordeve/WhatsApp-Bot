e34c9fe172794d5080b7dab7dc7143c9629030a5
naxor <naxordeve@gmail.com>
2025-03-23 22:09:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 require('dotenv').config();
 
 module.exports = {
-    prefix: new RegExp(process.env.PREFIX || '^[!/.#]'),
-    owner_num: process.env.OWNER_NUMBER || '27686881509',
-    mods: (process.env.MODS || '27686881509,2349112171078').split(',').filter(Boolean),
-    botName: process.env.BOT_NAME || 'X-Astral',
+    PREFIX: new RegExp(process.env.PREFIX || '^[!/.#?~]'),
+    OWNER_NUM: process.env.OWNER_NUMBER || '27686881509',
+    MODS: (process.env.MODS || '27686881509').split(',').filter(Boolean),
+    
+    BOT_NAME: process.env.BOT_NAME || 'X-Astral',
     
     GROK_API: process.env.GROK_API || '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',
 
     SESSION_ID: process.env.SESSION_ID || 'bWs3TVd_AFIYA_zRjE=',
-    packname: process.env.PACK_NAME || 'X ASTRAL',
-    author: process.env.AUTHOR || 'DIEGOSON',
- 
-    footer: process.env.FOOTER || '© wabot',
-    lang: process.env.LANG || 'en',
-    workType: process.env.WORK_TYPE || 'public'
+    
+    FOOTER: process.env.FOOTER || '© wabot',
+    LANG: process.env.LANG || 'en',
+    WORKTYPE: process.env.WORK_TYPE || 'private'
 };
 
 
