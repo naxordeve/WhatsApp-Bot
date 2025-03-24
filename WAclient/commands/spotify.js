d5cf7e0e22ea3ef050f0f675607a4f0c24a2ef3a
naxor <naxordeve@gmail.com>
2025-03-24 23:10:26 +0200

    Update spotify.js

--- WAclient/commands/spotify.js
+++ WAclient/commands/spotify.js
@@ -1,4 +1,3 @@
-
 const { Command } = require('../../lib/command');
 const axios = require('axios');
 
@@ -10,7 +9,6 @@ Command({
 })(async (msg, conn) => {
     const url = msg.text;
     if (!url) return msg.reply('Please provide Spotify URL');
-    msg.reply('wait...');
     const res = await axios.get(`https://api.vreden.my.id/api/spotify?url=${url}`);
     if (res.data && res.data.result) {
         const data = res.data.result;
@@ -20,27 +18,18 @@ Command({
             list += `${i + 1}. ${type}\n`;
         });
         list += '\n_Reply with the number to download_';
-        await msg.send({
-            image: { url: data.cover },
-            caption: list
+        await msg.send({ image: { url: data.cover }, caption: list
         });
         Command._ID_NUM(msg.sender, {
             callback: async (number, message) => {
                 if (number === 1) {
                     const audio = await axios.get(data.music, { responseType: 'arraybuffer' });
                     const buffer = Buffer.from(audio.data, 'binary');
-                    await message.send({
-                        document: buffer,
-                        mimetype: 'audio/mpeg',
-                        fileName: `${data.title}.mp3`,
-                        caption: `*Title:* ${data.title}\n*Artist:* ${data.artists}\n*Type:* ${data.type}\n*Release Date:* ${data.releaseDate}`
-                    });
+                    await message.send({document: buffer,mimetype: 'audio/mpeg',fileName: `${data.title}.mp3`,caption: `*Title:* ${data.title}\n*Artist:* ${data.artists}\n*Type:* ${data.type}\n*Release Date:* ${data.releaseDate}`});
                 } else {
                     const image = await axios.get(data.cover, { responseType: 'arraybuffer' });
                     const buffer = Buffer.from(image.data, 'binary');
-                    await message.send({
-                        image: buffer,
-                        caption: `*Title:* ${data.title}\n*Artist:* ${data.artists}\n*Type:* ${data.type}\n*Release Date:* ${data.releaseDate}`
+                    await message.send({image: buffer, caption: `*Title:* ${data.title}\n*Artist:* ${data.artists}\n*Type:* ${data.type}\n*Release Date:* ${data.releaseDate}`
                     });
                 }
             },
