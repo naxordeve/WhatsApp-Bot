74380c9a7d36fdc97e7ddc8857a151a1a23cac76
naxor <naxordeve@gmail.com>
2025-03-24 23:16:09 +0200

    Update worktype.js

--- WAclient/commands/worktype.js
+++ WAclient/commands/worktype.js
@@ -1,4 +1,3 @@
-
 const { Command } = require('../../lib/command');
 const config = require('../../config');
 const fs = require('fs');
@@ -6,33 +5,29 @@ const path = require('path');
 
 Command({
     cmd_name: 'worktype',
+    alias: ["mode"],
     category: 'owner',
     desc: 'Change bot worktype'
 })(async (msg, conn) => {
-    if (!msg.fromMe) return msg.reply('Only owner can use this command');
-    
+    if (!msg.fromMe) return;
     let list = '*Choose Worktype:*\n\n';
     const types = ['private', 'public'];
     types.forEach((type, i) => {
         list += `${i + 1}. ${type}\n`;
     });
-    list += '\n_Reply with the number to change worktype_';
+    list += '\n_Reply with the number_';
     await msg.reply(list);
-    
     Command._ID_NUM(msg.sender, {
         callback: async (number, message) => {
             const type = types[number - 1];
             config.WORKTYPE = type;
-            
-            // Update config.js file
-            const configPath = path.join(__dirname, '../../config.js');
-            let configContent = fs.readFileSync(configPath, 'utf8');
-            configContent = configContent.replace(
+            const kg = path.join(__dirname, '../../config.js');
+            let cmd = fs.readFileSync(kg, 'utf8');
+            cmd = cmd.replace(
                 /WORKTYPE:\s*process\.env\.WORK_TYPE\s*\|\|\s*'[^']*'/,
                 `WORKTYPE: process.env.WORK_TYPE || '${type}'`
             );
-            fs.writeFileSync(configPath, configContent);
-            
+            fs.writeFileSync(kg, cmd);
             await message.reply(`*Worktype changed to:* ${type}`);
         },
         valid: [1, 2]
