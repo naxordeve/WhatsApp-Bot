17ee5d8d1e9202d3c1494306bac70385cbf347e7
naxordevi <109889758+Diegoson@users.noreply.github.com>
2025-03-27 13:11:52 +0200

    Update igdl.js

--- WAclient/commands/igdl.js
+++ WAclient/commands/igdl.js
@@ -5,18 +5,16 @@ const axios = require('axios');
 Command({
   cmd_name: 'igdl',
   aliases: ['instadl'],
-  category: 'download',
+  category: 'media',
   desc: 'Download Instagram post/reel '
 })(async (msg) => {
   const url = extractUrl(msg.text);
-  if (!url) return msg.reply('_Please provide Instagram url_');
+  if (!url && msg.quoted) {
+      url = extractUrl(msg.quoted.message?.conversation || msg.quoted.message?.extendedTextMessage?.text || '');
+  }if (!url) return msg.reply('_Please provide Instagram url_');
   const res = await axios.get(`https://diegoson-naxordeve.hf.space/instagram/v5?url=${url}`);
   if (!res.data || !res.data.downloadUrls) return;
   const data = res.data;
-  await msg.send({
-    image: { url: data.thumbnail },
-    caption: `*${data.title}*\n\n_Wait..._`
-  });
-
+  await msg.send({image: { url: data.thumbnail }, caption: `*${data.title}*\n\n_Wait..._` });
   await msg.send({ video: { url: data.downloadUrls[0] }, caption: '*X ASTRAL*' });
 });
