d204aa431180fd092a238c5ae7d482683621b9fa
naxordevi <109889758+Diegoson@users.noreply.github.com>
2025-03-27 21:20:35 +0200

    Create y2mate.js

---
+++ WAclient/commands/y2mate.js
@@ -0,0 +1,31 @@
+const { Command } = require('../../lib/command');
+var { extractUrl } = require('../../lib/Functions');
+const axios = require('axios');
+
+Command({
+    cmd_name: 'ytmp3',
+    aliases: ['y2mate'],
+    category: 'downloader',
+    desc: 'Download YouTube audio'
+})(async (msg) => {
+    var url = extractUrl(msg.text);
+    if (url) return msg.reply('*_Please provide a YouTube url_*');
+    const avoidable = `https://diegoson-naxordeve.hf.space/api/download?url=${url}`;
+    const res = await axios.get(avoidable);
+    const data = res.data;
+    await msg.reply(`*_Now Playing* ${data.title}.._`);
+    await msg.send({ audio: { url: data.audio['320'] }, mimetype: 'audio/mpeg', fileName: `${data.title}.mp3`,
+        contextInfo: {
+            externalAdReply: {
+                title: data.title,
+                body: `Duration: ${data.duration_time}`,
+                thumbnailUrl: data.thumbnail,
+                sourceUrl: data.youtube_url,
+                mediaType: 1,
+                showAdAttribution: true,
+                renderLargerThumbnail: true
+            }
+        }
+    });
+});
+   
