77498921fa5e4a454bbedeeeb5cd0b4c0ddd54b5
naxordevi <109889758+Diegoson@users.noreply.github.com>
2025-03-27 22:25:22 +0200

    Update and rename block.js to owner.js

---
+++ WAclient/commands/owner.js
@@ -0,0 +1,28 @@
+var { Command } = require('../../lib/command');
+
+Command({
+    cmd_name: 'block',
+    category: 'owner',
+    desc: 'Block a user'
+})(async (msg, conn) => {
+    if(!msg.fromMe) return;
+    if (!msg.quoted && !msg.text) return msg.reply('_Tag someone or reply to someone_');
+    const xxx = msg.quoted ? msg.quoted.sender : msg.text.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
+    await conn.updateBlockStatus(xxx, "block")
+        .then(() => msg.reply('*_Blocked sucss_*'))
+        .catch(() => msg.reply('oops'));
+});
+
+Command({
+    cmd_name: 'unblock',
+    category: 'owner',
+    desc: 'Unblock a user'
+})(async (msg) => {
+    if(!msg.fromMe) return;
+    if (!msg.quoted && !msg.text) return msg.reply('Tag someone or reply to their message to unblock them');
+    const xxx = msg.quoted ? msg.quoted.sender : msg.text.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
+    await conn.updateBlockStatus(xxx, "unblock")
+        .then(() => msg.reply('*_Unblocked_*'))
+        .catch(() => msg.reply('oops'));
+});
+
