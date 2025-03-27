7a2dbaddf187b09bb6fe3a4ea6bea520525807a1
naxordevi <109889758+Diegoson@users.noreply.github.com>
2025-03-27 21:47:45 +0200

    Update y2mate.js

--- WAclient/commands/y2mate.js
+++ WAclient/commands/y2mate.js
@@ -11,21 +11,13 @@ Command({
     var url = extractUrl(msg.text);
     if (!url && msg.quoted) {
       url = extractUrl(msg.quoted.message?.conversation || msg.quoted.message?.extendedTextMessage?.text || '');
-   }if (url) return msg.reply('*_Please provide a YouTube url_*');
+   }if (!url) return msg.reply('*_Please provide a YouTube url_*');
     const avoidable = `https://diegoson-naxordeve.hf.space/api/download?url=${url}`;
     const res = await axios.get(avoidable);
     const data = res.data;
-    await msg.reply(`*_Now Playing* ${data.title}.._`);
+    await msg.reply(`*_Now Playing ${data.title}.._*`);
     await msg.send({ audio: { url: data.audio['320'] }, mimetype: 'audio/mpeg', fileName: `${data.title}.mp3`,
-        contextInfo: {
-            externalAdReply: {
-                title: data.title,
-                body: `Duration: ${data.duration_time}`,
-                thumbnailUrl: data.thumbnail,
-                sourceUrl: data.youtube_url,
-                mediaType: 1,
-                showAdAttribution: true,
-                renderLargerThumbnail: true
+    contextInfo: {externalAdReply: { title: data.title, body: `Duration: ${data.duration_time}`, thumbnailUrl: data.thumbnail, sourceUrl: data.youtube_url, mediaType: 1, showAdAttribution: true, renderLargerThumbnail: true
             }
         }
     });
