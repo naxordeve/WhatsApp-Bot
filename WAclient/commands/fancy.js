b9c9981b09d38c5948efcfc6f6ce78c63fe8e2f9
naxordevi <109889758+Diegoson@users.noreply.github.com>
2025-03-28 10:59:20 +0200

    Update and rename fancy (1).js to fancy.js

--- WAclient/commands/fancy.js
+++ WAclient/commands/fancy.js
@@ -7,17 +7,14 @@ Command({
     aliases: ['style', 'stylish'],
     category: 'converter',
     desc: 'Convert text to fancy styles'
-})(async (msg, args, client) => {
-    if (!args.length) {
-        return msg.reply(listStyles());
-    }
-
-    const style = parseInt(args[0]);
+})(async (msg) => {
+    if (!msg.text.length) return msg.reply(listStyles());
+    const style = parseInt(msg.text);
     if (!isNaN(style) && style >= 1 && style <= 20) {
-        const text = args.slice(1).join(' ');
-        if (!text) return msg.reply('Please provide text to convert!');
+        const text = msg.text;
+        if (!text) return msg.reply('Please provide text to convert');
         return msg.reply(getFancyText(text, style));
     }
 
-    return msg.reply(getFancyText(args.join(' ')));
+    return msg.reply(getFancyText(msg.text));
 });
